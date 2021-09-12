bb2758bbafdb953615b725c4de803adc6383477b
vodkar <boombarah@mail.ru>
2021-09-12 15:51:56 +0500

    Add config

---
+++ game_of_life/config.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""Configuration file"""
+
+__author__ = "vodkar"
+
+from dataclasses import dataclass
+from enum import Enum
+
+from yaml import load, SafeLoader
+
+
+CONFIG_FILE = "config.yaml"
+SEED_CELLS = "seed_cells"
+HEIGHT_FIELD = "height"
+WIDTH_FIELD = "width"
+DRAWER_FIELD = "drawer"
+
+
+@dataclass(frozen=True)
+class CellConfig:
+    height: int
+    width: int
+
+
+@dataclass(frozen=True)
+class LifeConfig:
+    height: int
+    width: int
+    grid_drawer: str
+    seed_cells: list[CellConfig]
+
+
+def load_config() -> LifeConfig:
+    """
+    Load config from CONFIG_FILE
+    """
+    config = load(open(CONFIG_FILE, "r+", encoding="utf-8"), SafeLoader)
+    cells = [CellConfig(cell[HEIGHT_FIELD], cell[WIDTH_FIELD]) for cell in config.get(SEED_CELLS, [])]
+    return LifeConfig(config[HEIGHT_FIELD], config[WIDTH_FIELD], config[DRAWER_FIELD], cells)
--- main.py
+++ main.py
@@ -5,3 +5,4 @@
 __author__ = "vodkar"
 
 from game_of_life.models import Grid, LifeGame
+from game_of_life.config import load_config
